140901f364bdfa18e02329d14dfdf79294db5f87
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-10-17 20:51:59 -0400

    Bump version to 0.4.0.

--- discord.tcl
+++ discord.tcl
@@ -19,7 +19,7 @@ namespace eval discord {
     namespace export connect disconnect setCallback
     namespace ensemble create
 
-    variable version 0.3.2
+    variable version 0.4.0
 
     variable log [::logger::init discord]
     ${log}::setlevel debug
--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -8,7 +8,7 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded discord 0.3.2 "
+package ifneeded discord 0.4.0 "
     source [file join $dir gateway.tcl] ;
     source [file join $dir callback.tcl] ;
     source [file join $dir disrest.tcl] ;
