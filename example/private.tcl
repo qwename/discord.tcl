71f265a31476081d5b01159d685f5174ec854466
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-10-18 23:10:23 -0400

    Updated information for private.tcl.

--- example/private.tcl
+++ example/private.tcl
@@ -1,2 +1,5 @@
+# To untrack this file use the following command.
+# git update-index --assume-unchanged example/private.tcl
+
 set ownerId 000000000000000000
 set token "Replace with your token here."
