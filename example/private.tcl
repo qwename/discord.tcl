86cd189baa95f3ea270504db6d15a41c629ee149
Yixin Zhang <qwename@users.noreply.github.com>
2016-10-19 21:10:35 -0400

    Deleted private.tcl remnant.

--- example/private.tcl
+++
@@ -1,5 +0,0 @@
-# To untrack this file use the following command.
-# git update-index --assume-unchanged example/private.tcl
-
-set ownerId 000000000000000000
-set token "Replace with your token here."
